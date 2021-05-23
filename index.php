5a688598f7212ef50633583d6d1c99cc0d092908
Nam <hoangnam12c12@gmail.com>
2021-05-23 22:08:39 +0700

    add php file

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-<?php include_once("index.html"); ?>
+<?php 
+    include("index.html"); 
+    $conn = new mysqli('localhost', 'root', '', 'Blackjack'); 
+    if ($conn->connect_error) {
+        exit; 
+    }
+    $sql = "SELECT * FROM Player"; 
+    $result = $conn->query($sql); 
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            echo "Username: " . $row['Username'] . "<br>"; 
+        }
+    }
+    else {
+        echo "No result"; 
+    }
+
+?>
